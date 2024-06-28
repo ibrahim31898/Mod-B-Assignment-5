9ac59cabf5f36abc242f0710a31bc6606ff90f0b
Muhammad Ibrahim Jan <ibrahimjan31898@gmail.com>
2024-06-28 10:13:51 +0500

    Add work to github repo

---
+++ firstreactapp/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+const Heading = ()=>{
+  return(
+    <div>
+      <h1>Hello World</h1>
+      <p>Welcome to the React Assignment</p>
+      <img src="https://miro.medium.com/v2/resize:fit:1280/0*Vi7dVWLLqDhCsEkW" alt="" />
+    </div>
+  )
+}
+
+export default Heading;
